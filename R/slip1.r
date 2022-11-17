7ddd0a78c33c545dd30c26e481be64a284d9bde7
sanketbodke <sanketbodke26@gmail.com>
2022-11-17 18:18:02 +0530

    r problem add

---
+++ R/slip1.r
@@ -0,0 +1,5 @@
+nums = c(10, 20, 30, 40, 50, 60)
+print('Original vector:')
+print(nums)   
+print(paste("Maximum value of the said vector:",max(nums)))
+print(paste("Minimum value of the said vector:",min(nums)))
